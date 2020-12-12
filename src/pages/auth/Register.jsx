c839817a37228c021fffdd45d2c213651ae53daf
pekstaar <pekstaar@gmail.com>
2020-12-12 17:05:07 +0300

    register-form

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { Input } from "antd";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  };
+
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <Input
+        style={{ height: "3.5em" }}
+        type="email"
+        placeholder="Enter your email"
+        className="form-control"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        autoFocus
+      />
+      
+      <button type="submit" className="btn btn-raised btn-secondary m-2">Register</button>
+    </form>
+  );
+
   return (
-    <div>
-      <p>Register</p>
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h5>SIGN-UP</h5>
+          {registerForm()}
+        </div>
+      </div>
     </div>
   );
 };
