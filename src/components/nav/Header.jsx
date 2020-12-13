18ca1c4a1ac3debe2a568b241080e22b8d959c04
pekstaar <pekstaar@gmail.com>
2020-12-13 12:44:54 +0300

    logout

--- src/components/nav/Header.jsx
+++ src/components/nav/Header.jsx
@@ -1,17 +1,39 @@
 import { Menu } from "antd";
-import { HomeOutlined, LoginOutlined, SettingOutlined, UserAddOutlined } from "@ant-design/icons";
+import {
+  HomeOutlined,
+  LoginOutlined,
+  LogoutOutlined,
+  SettingOutlined,
+  UserAddOutlined,
+} from "@ant-design/icons";
+import firebase from "firebase";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 //destructuring
-const { SubMenu,Item } = Menu;
+const { SubMenu, Item } = Menu;
 
 const Header = () => {
   const [current, setCurrent] = useState("home");
 
+  const dispatch = useDispatch();
+  const history = useHistory();
+
   const handleClick = (e) => {
     // console.log();
     setCurrent(e.key);
+  };
+
+  const logout = () => {
+    firebase.auth().signOut();
+
+    dispatch({
+      type: "LOGOUT",
+      payload: null, //make state empty
+    });
+
+    history.push("/login");
   };
 
   return (
@@ -19,16 +41,21 @@ const Header = () => {
       <Item key="home" icon={<HomeOutlined />}>
         <Link to="/">Home</Link>
       </Item>
+
       <Item key="register" icon={<UserAddOutlined />} className="float-right">
-      <Link to="/register">Register</Link>
+        <Link to="/register">Register</Link>
       </Item>
+
       <Item key="login" icon={<LoginOutlined />} className="float-right">
-      <Link to="/login">Login </Link>
+        <Link to="/login">Login </Link>
       </Item>
 
       <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
-          <Item key="setting:1">Option 1</Item>
-          <Item key="setting:2">Option 2</Item>
+        <Item key="setting:1">Option 1</Item>
+        <Item key="setting:2">Option 2</Item>
+        <Item icon={<LogoutOutlined />} onClick={logout}>
+          Logout
+        </Item>
       </SubMenu>
     </Menu>
   );
