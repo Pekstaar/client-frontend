c355c2d2ad825517d76c14e6b46cf756f30599c0
pekstaar <pekstaar@gmail.com>
2021-01-05 22:13:57 +0300

    new

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-// all reducers combination
-
-import { combineReducers } from "redux";
-import { userReducer } from "./userReducer";
-
-export const rootReducer = combineReducers({
-  user: userReducer,
-});
